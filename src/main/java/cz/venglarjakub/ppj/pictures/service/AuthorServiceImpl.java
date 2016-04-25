748a969914e64136dfa0d82a84cd90600303eb11
Jakub_Venglar <jakub.venglar@tul.cz>
2016-04-25 15:31:19 +0200

    service layer

--- src/main/java/cz/venglarjakub/ppj/pictures/service/AuthorServiceImpl.java
+++ src/main/java/cz/venglarjakub/ppj/pictures/service/AuthorServiceImpl.java
@@ -3,7 +3,7 @@ package cz.venglarjakub.ppj.pictures.service;
 import org.springframework.stereotype.Service;
 
 /**
- * Created by kuba on 25. 4. 2016.
+ * Created by Jakub Venglar on 25. 4. 2016.
  */
 @Service
 public class AuthorServiceImpl implements AuthorService {
